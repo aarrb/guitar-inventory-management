14de2ef48ddd515ba59fc740512888499e90a63b
French II, Stephan Floyd <stffrenc@iu.edu>
2024-01-26 13:23:09 -0500

    Wrote Tests

--- src/test/java/edu/iu/aurabaza/Homework2/repository/InventoryRepositoryTest.java
+++ src/test/java/edu/iu/aurabaza/Homework2/repository/InventoryRepositoryTest.java
@@ -1,11 +1,13 @@
 package edu.iu.aurabaza.Homework2.repository;
 
+import edu.iu.aurabaza.Homework2.model.Guitar;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class InventoryRepositoryTest {
@@ -32,10 +34,25 @@ class InventoryRepositoryTest {
 
     @Test
     void getGuitar() {
-
+        InventoryRepository inventoryRepository = new InventoryRepository();
+        InventoryRepository.addGuitar("19111908", 500.0, "Austin", "Model1", "Electric", "Sycamore", "Maple");
+        Guitar expectedGuitar = new Guitar("19111908", 500.0, "Austin", "Model1", "Electric", "Sycamore", "Maple");
+        Guitar actualGuitar = inventoryRepository.getGuitar("19111908");
+        assertEquals(expectedGuitar.getSerialNumber(), actualGuitar.getSerialNumber());
+        assertEquals(expectedGuitar.getBuilder(), actualGuitar.getBuilder());
+        assertEquals(expectedGuitar.getType(), actualGuitar.getType());
+        assertEquals(expectedGuitar.getModel(), actualGuitar.getModel());
+        assertEquals(expectedGuitar.getBackWood(), actualGuitar.getBackWood());
+        assertEquals(expectedGuitar.getTopWood(), actualGuitar.getTopWood());
+        assertEquals(expectedGuitar.getPrice(), actualGuitar.getPrice());
     }
 
     @Test
     void search() {
+        Guitar criteriaGuitar = new Guitar(null, 500.0, null, null, null, "Sycamore", "Maple");
+        InventoryRepository inventoryRepository = new InventoryRepository();
+        inventoryRepository.search(criteriaGuitar).toString();
+        List<Guitar> actualList = new ArrayList<Guitar>();
+        assertArrayEquals(inventoryRepository.search(criteriaGuitar).toArray(), actualList.toArray());
     }
 }
