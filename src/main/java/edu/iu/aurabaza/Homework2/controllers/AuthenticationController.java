2546af4d9b40457d8991b57ad90d0f77ec2c47ac
French II, Stephan Floyd <stffrenc@iu.edu>
2024-04-14 16:51:08 -0400

    CORS Change again

--- src/main/java/edu/iu/aurabaza/Homework2/controllers/AuthenticationController.java
+++ src/main/java/edu/iu/aurabaza/Homework2/controllers/AuthenticationController.java
@@ -8,7 +8,7 @@ import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE}, allowCredentials = "true")
 public class AuthenticationController {
 
     private final AuthenticationManager authenticationManager;
